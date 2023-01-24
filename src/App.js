f47ea2a4ac0a97ddc25de431db33ff7bbb707d6e
Miles <webdevmiles@gmail.com>
2023-01-24 22:45:15 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./store/actions/counter.actions";
+import { logginUser } from "./store/actions/login.action";
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+
+
+  console.log(isLogged)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {( isLogged && 
+      <>
+      <h1>{counter}</h1>
+      <h1>{isLogged}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      </>
+      )}
+      <button onClick={() => dispatch(logginUser())}>Login</button>
+
     </div>
   );
 }
